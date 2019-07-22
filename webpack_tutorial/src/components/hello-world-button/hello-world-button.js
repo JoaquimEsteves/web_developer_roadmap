783bdc4a811df42d9e0f239e63a977589662d447
XBXD5499 <joaquim.esteves.ext@orange.com>
2019-07-22 17:37:53 +0200

    Further progress on webpack tutorial

--- webpack_tutorial/src/components/hello-world-button/hello-world-button.js
+++ webpack_tutorial/src/components/hello-world-button/hello-world-button.js
@@ -1,5 +1,17 @@
-function helloWorld(){
-    console.log('Hello World');
-}
+import './hello-world-button.css';
 
-export default helloWorld;
+export class HelloWorldButton {
+    render() {
+        const button = document.createElement('button');
+        button.innerHTML = 'Hello World';
+        button.classList.add('hello-world-button');
+        const body = document.querySelector('body');
+        body.onclick = () => {
+            const p = document.createElement('p');
+            p.innerHTML = 'Hello World';
+            p.classList.add('hello-world-paragraph');
+            body.appendChild(p);
+        }
+        body.appendChild(button);
+    }
+}
--- webpack_tutorial/webpack.config.js
+++ webpack_tutorial/webpack.config.js
@@ -10,10 +10,18 @@ module.exports = {
     mode: 'none',
     module: {
         rules: [{
-            test: /\.(png|jpg)$/,
-            use: [
-                'file-loader'
-            ]
-        }]
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 }
